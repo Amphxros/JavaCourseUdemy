263dcc692d7b04399a8edf30eff8c4c16cc70c44
Amph <amprubio@ucm.es>
2020-09-15 12:33:03 +0200

    tercera clase

---
+++ Introduccion/src/condicionales.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class condicionales {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		Scanner tec= new Scanner(System.in);
+		String txt= "ingrese un valor";
+		
+		int n;
+		
+		//programa
+		
+		System.out.println(txt);
+		n= tec.nextInt();
+	
+		if(n%2==0 && n!=0) {
+			System.out.println("es un num par");
+		}
+		else {
+			System.out.println("no es un num par");
+		}
+	
+	}
+
+}
